8e6df0ad5fe35ef3488dca96c1d95402775307a6
gurmeher <gurmeher@berkeley.edu>
2025-10-21 23:12:54 -0700

    feat: added pfp

--- app/header.tsx
+++ app/header.tsx
@@ -1,23 +1,35 @@
 'use client'
 import { TextEffect } from '@/components/ui/text-effect'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export function Header() {
   return (
     <header className="mb-8 flex items-center justify-between">
-      <div>
-        <Link href="/" className="text-xl font-semibold text-black dark:text-white">
-          Gurmeher Bhasin
-        </Link>
-        <TextEffect
-          as="p"
-          preset="fade"
-          per="char"
-          className="text-lg text-zinc-600 dark:text-zinc-300"
-          delay={0.5}
-        >
-          Computer Science, Data Science @ UC Berkeley
-        </TextEffect>
+      <div className="flex items-center gap-4">
+        <div className="relative h-16 w-16 flex-shrink-0">
+          <Image
+            src="/profile-photo.jpeg"
+            alt="Gurmeher Bhasin"
+            width={64}
+            height={64}
+            className="rounded-full object-cover"
+          />
+        </div>
+        <div>
+          <Link href="/" className="text-xl font-semibold text-black dark:text-white">
+            Gurmeher Bhasin
+          </Link>
+          <TextEffect
+            as="p"
+            preset="fade"
+            per="char"
+            className="text-lg text-zinc-600 dark:text-zinc-300"
+            delay={0.5}
+          >
+            Computer Science, Data Science @ UC Berkeley
+          </TextEffect>
+        </div>
       </div>
     </header>
   )
